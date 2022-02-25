c80569929f1fb794a5ea3f300581c5345cb749fd
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-02-25 15:03:14 +0200

    use CBV for post-list

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -15,7 +16,14 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',
-                  {'posts': posts, 'page': page})
+                  {'posts': posts, 'page_obj': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
